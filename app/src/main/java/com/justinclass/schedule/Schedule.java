d4a4ba42450481596214318ba0e58120ef042024
Alfredo De Goyeneche <asdegoyeneche@uc.cl>
2018-01-21 10:58:07 -0800

    parsing and path

--- app/src/main/java/com/justinclass/schedule/Schedule.java
+++ app/src/main/java/com/justinclass/schedule/Schedule.java
@@ -10,18 +10,107 @@ import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
+//import com.justinclass.schedule.Meeting;
 /**
  * Created by Kimi on 1/21/2018.
  */
 
 public class Schedule {
     List<Course> DCourseList;
+    List<Meeting> DMeetingList;
     String DUniversityName;
     String DCityName;
 
     public Schedule(){
         DCourseList = new ArrayList<Course>();
+    }
+
+    public void LoadSchedule(){
+        try {
+            String path = System.getProperty("user.dir");
+            path = path.split("src")[0] + "sampledata";
+            File file = new File(path);
+            BufferedReader b = new BufferedReader(new FileReader(file));
+            Boolean first = Boolean.TRUE;
+            DMeetingList = new ArrayList<Meeting>();
+            Meeting meeting = new Meeting();
+            for(String line; (line = b.readLine()) != null; ) {
+                line = line.trim();
+                System.out.println(line);
+
+                if(line.contains("BEGIN:VEVENT")){
+                    if(!first){
+                        DMeetingList.add(meeting);
+                    }
+                    first = Boolean.FALSE;
+                    meeting = new Meeting();
+                }
+                else if(line.contains("DTSTART")){
+                    if(line.contains("DTSTART;")){
+                        line = line.split("=")[1];
+                    }
+                    line = line.split(":")[1];
+                    String date = line.split("T")[0];
+                    String time = line.split("T")[1];
+                    meeting.DStartTime.set(Integer.parseInt(date.substring(0,4)),Integer.parseInt(date.substring(4,6)),Integer.parseInt(date.substring(6,8)), Integer.parseInt(time.substring(0,2)), Integer.parseInt(time.substring(2,4)),Integer.parseInt(time.substring(4,6)));
+
+                }
+                else if(line.contains("DTEND")){
+                    if(line.contains("DTEND;")){
+                        line = line.split("=")[1];
+                    }
+                    line = line.split(":")[1];
+                    String date = line.split("T")[0];
+                    String time = line.split("T")[1];
+                    meeting.DEndTime.set(Integer.parseInt(date.substring(0,4)),Integer.parseInt(date.substring(4,6)),Integer.parseInt(date.substring(6,8)), Integer.parseInt(time.substring(0,2)), Integer.parseInt(time.substring(2,4)),Integer.parseInt(time.substring(4,6)));
+
+                }
+                else if(line.contains("LOCATION:")){
+                    meeting.DLocation = line.split(":")[1];
+                }
+                else if(line.contains("SUMMARY:")){
+                    meeting.DCourse = line.split(":")[1];
+                }
+                else if(line.contains("RRULE:")){
+                    line = line.split(":")[1];
+                    meeting.DFrequency = Integer.parseInt(line.split(";")[0].split("=")[1]);
+                    meeting.DCount = Integer.parseInt(line.split(";")[1].split("=")[1]);
+                    for(String day : line.split(";")[2].split("=")[1].split(",")){
+                        if(day == "MO"){
+                            meeting.DDays[Calendar.MONDAY-1] = Boolean.TRUE;
+                        }else if(day == "TU"){
+                            meeting.DDays[Calendar.TUESDAY-1] = Boolean.TRUE;
+
+                        }else if(day == "WE"){
+                            meeting.DDays[Calendar.WEDNESDAY-1] = Boolean.TRUE;
+
+                        }else if(day == "TH"){
+                            meeting.DDays[Calendar.THURSDAY-1] = Boolean.TRUE;
+
+                        }else if(day == "FR"){
+                            meeting.DDays[Calendar.TUESDAY-1] = Boolean.TRUE;
+
+                        }else if(day == "SA"){
+                            meeting.DDays[Calendar.SATURDAY-1] = Boolean.TRUE;
+
+                        }else if(day == "SU"){
+                            meeting.DDays[Calendar.SUNDAY-1] = Boolean.TRUE;
+
+                        }
+                    }
+                }
+
+
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Meeting nextMeeting(){
@@ -73,7 +162,4 @@ public class Schedule {
         return nextMeeting;
     }
 
-    public void LoadSchedule(){
-
-    }
 }

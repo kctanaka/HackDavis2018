d9c3535102c7e66a758e0a5705fc080f3fcf5a6d
kctanaka <kctanaka@ucdavis.edu>
2018-01-20 18:36:25 -0800

    Starter files?

---
+++ app/src/main/java/com/justinclass/MainActivity.java
@@ -0,0 +1,45 @@
+package com.justinclass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.location.*;
+import android.app.*;
+import android.content.*;
+import android.provider.Settings;
+
+public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+
+    /* from https://www.androidauthority.com/get-use-location-data-android-app-625012/ :  */
+    private boolean isLocationEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable Location")
+                .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " +
+                        "use this app")
+                .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    }
+                });
+        dialog.show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/justinclass/StartScreen.java
@@ -0,0 +1,25 @@
+package com.justinclass;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+
+    protected launchMain(View view){
+            Intent intent = new Intent(this, MainActivity.class);
+            //String message = editText.getText().toString();
+            //intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        }
+
+    }
+
+}
